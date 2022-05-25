5af1dc44f08c17d240d934781780e23e1440548a
pablop442 <89651989+pablop442@users.noreply.github.com>
2022-05-25 11:26:41 +0200

    added files to docs folder

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TodoList from '../src/Components/ToDoList';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TodoList />
+  </React.StrictMode>
+);
+
+
